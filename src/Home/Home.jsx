5cb9b70c7149a7f8d75baff881a78b6f1e15063d
jRicardoAFS <joaoricardoss.2005@gmail.com>
2024-04-16 14:43:34 -0300

    add CategoryShowCase;
    add shape;

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
 import HomeCategory from './HomeCategory'
+import Category from './CategoryShowCase'
 
 const Home = () => {
   return (
     <div>
       <Banner/>
       <HomeCategory/>
+      <Category/>
     </div>
   )
 }
