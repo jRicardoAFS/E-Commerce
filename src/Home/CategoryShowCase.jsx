ed45a97d33b97dda19b0d54c81a712a08432111f
jRicardoAFS <joaoricardoss.2005@gmail.com>
2024-04-16 15:44:13 -0300

    add section body; add Ratting;

--- src/Home/CategoryShowCase.jsx
+++ src/Home/CategoryShowCase.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
+import Ratting from '../components/Ratting';
+import { Link } from 'react-router-dom';
 
 const title = "Our Products";
 
 const ProductData = [
     {
-        imgUrl: 'src/assets/images/CategoryShowCaseTab/01.jpg',
+        imgUrl: 'src/assets/images/categoryTab/01.jpg',
         cate: 'Shoes',
         title: 'Nike Premier X',
         author: 'assets/images/course/author/01.jpg',
@@ -80,8 +82,11 @@ const ProductData = [
 const CategoryShowCase = () => {
     const [items, setItems] = useState(ProductData)
 
-    const filterItem = () => {
-
+    const filterItem = (categItem) => {
+        const updateItems = ProductData.filter((curElem)=>{
+            return curElem.cate === categItem
+        })
+        setItems(updateItems)
     }
     return (
 
@@ -98,7 +103,7 @@ const CategoryShowCase = () => {
                     <h2 className="title">{title}</h2>
                     <div className='course-filter-group'>
                         <ul className="lab-ul">
-                            <li onClick={() => filterItem("All")}>All</li>
+                            <li onClick={() => setItems(ProductData)}>All</li>
                             <li onClick={() => filterItem("Shoes")}>Shoes</li>
                             <li onClick={() => filterItem("Bags")}>Bags</li>
                             <li onClick={() => filterItem("Phones")}>Phones</li>
@@ -108,7 +113,44 @@ const CategoryShowCase = () => {
 
                 </div>
 
-                
+                <div className="section-wrapper">
+                    <div className='row g-4 justify-content-center row-cols-xl-4 row-cols-lg-3 row-cols-md-2 row-cols-1 course-filter'> 
+                        {
+                            items.map((product) => <div key={product.id} className='col'>
+                                <div className="course-item style-4">
+                                    <div className="course-inner">
+                                        <div className="course-thumb">
+                                            <img src={product.imgUrl} alt="" />
+                                            <div className="course-category">
+                                                <div className="course-cate">
+                                                    <a href="#">{product.cate}</a>
+                                                </div>
+                                                <div className="course-reiew">
+                                                    <Ratting />
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="course-content">
+                                            <Link to={`/shop/${product.id}`}>{product.title}</Link>
+                                            <div className="course-footer">
+                                                <div className="course-author">
+                                                    <Link to="/" className='ca-name'>{product.brand}</Link>
+                                                </div>
+                                                <div className="course-price">
+                                                    {product.price}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>)
+
+                        }
+
+                    </div>
+                </div>
             </div>
         </div>
     )
